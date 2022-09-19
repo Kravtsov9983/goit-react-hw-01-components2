b3189a7eaca1b1709d7c8dd0143d6ef76722ff7b
Kravtsov <kravtsovofficial99@gmail.com>
2022-09-19 16:33:32 +0300

    finish

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,23 +1,35 @@
-import FriendListMenu from "../FriendList/FriendListMenu"
+import React from 'react';
+import css from './FriendList.module.css'
+import  FriendListItem from './FriendListItem/FriendListItem'
+import PropTypes from 'prop-types';
 
-
-export default function FriendList({ friends }) {
-    const elements = friends.map(({ avatar, name, isOnline, id }) => 
-        <>
-        
-            <FriendListMenu key={id} avatar={avatar} name={name} isOnline={isOnline} />
-        </>
-        )
-   {
-        return (
-            
-                <ul className="friend-list">{ elements }</ul   >
-        
-        
+const FriendList = ({ friends }) => {
+    return (
+        <ul className={css.friendList}>
+            {friends.map(friend => {
+                return (
+                    <FriendListItem
+                        key={friend.id}
+                        avatar={friend.avatar}
+                        name={friend.name}
+                        isOnline={friend.isOnline}
+                    />
+                )
+            })}
+        </ul>
     )
-  }
 }
 
+FriendList.protoTypes = {
+    friends: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            avatar: PropTypes.string.isRequired,
+            name: PropTypes.string.isRequired,
+            isOnline: PropTypes.bool.isRequired
+        })
+    ).isRequired,
+};
 
-
+export default FriendList
 
